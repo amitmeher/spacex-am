d131f557ed38245bdfcc265f1a0645dc9f117b8a
Amit Meher <amitmeher567@gmail.com>
2020-09-12 04:09:22 +0530

    Updates

---
+++ precache-manifest.cf146cf48f6a0ac1558b8ae70a272117.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd50036003ac482f69a617eb6071c61c",
+    "url": "/spacex-am/index.html"
+  },
+  {
+    "revision": "8371fbdbef8199cf463f",
+    "url": "/spacex-am/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4c8a166ed0ba5e652799",
+    "url": "/spacex-am/static/js/2.efa8a74a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spacex-am/static/js/2.efa8a74a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8371fbdbef8199cf463f",
+    "url": "/spacex-am/static/js/main.f3c6d6fa.chunk.js"
+  },
+  {
+    "revision": "710fe37bcdab0dc96d53",
+    "url": "/spacex-am/static/js/runtime-main.16a1211a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spacex-am/static/media/logo.5d5d9eef.svg"
+  }
+]);
